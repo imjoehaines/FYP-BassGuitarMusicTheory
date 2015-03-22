8c5298f618aee68f5d812aad80c5bf7c76f28a27
Joe Haines <hello@joehaines.co.uk>
2015-03-22 15:37:14 +0000

    Converson of notes exercise to KineticJS done

--- js/exercises/notes.js
+++ js/exercises/notes.js
@@ -17,7 +17,11 @@ var stage = new Kinetic.Stage({
 
 var circleLayer = new Kinetic.Layer();
 
-function startExercise() {
+// empty function as button clicks wont do anything in this exercise
+// TODO: fix this
+function buttonClicked() {}
+
+function drawRandomNote() {
     // Intersections are at x = (50 + ((stringLength / MAX_FRETS) / 2)) + (((stringLength - 50) / MAX_FRETS)
     // y values are 50, 100, 150, 200 for G, D, A, E strings
 
@@ -36,107 +40,106 @@ function startExercise() {
     );
 
     currentNote = NOTES[string][fret];
+
+    stage.add(circleLayer);
 }
 
-function buttonClicked() {
-    
+function answerButton(link) {
+    if (exerciseIsRunning) {
+
+        var answer = link.innerHTML;
+
+        if (currentNote == answer) {
+            score += 1;
+            FEEDBACK_DISPLAY.innerHTML = "Correct!";
+        } else {
+            FEEDBACK_DISPLAY.innerHTML = "Incorrect!";
+        }
+
+        displayFeedback();
+        totalQuestions += 1;
+        TOTAL_DISPLAY.innerHTML = totalQuestions;
+        SCORE_DISPLAY.innerHTML = Math.round((score / totalQuestions) * 100);
+        CORRECT_DISPLAY.innerHTML = score;
+
+        // remove old drawn notes
+        circleLayer.removeChildren();
+
+        drawRandomNote();
+    }
 }
+
+function displayFeedback() {
+    var classToAdd;
+    var timeout = 1250;
+
+    if (FEEDBACK_DISPLAY.innerHTML == "Correct!") {
+        classToAdd = "correctAnswer";
+    } else {
+        classToAdd = "incorrectAnswer";
+    }
+
+    setTimeout(function() {
+        FEEDBACK_DIV.classList.remove(classToAdd);
+        FEEDBACK_DIV.style.opacity = 0;
+    }, timeout);
+
+    FEEDBACK_DIV.style.opacity = 1;
+    FEEDBACK_DIV.classList.add(classToAdd);
+}
+
+function updateTimer() {
+    var extraZero = 0;
+
+    timerSeconds -= 1;
+
+    if (timerSeconds < 0) {
+        timerMinutes -= 1;
+        timerSeconds = 59;
+    }
+
+    if (timerSeconds < 10) {
+        extraZero = 0;
+    } else {
+        extraZero = "";
+    }
+
+    TIMER_DISPLAY.innerHTML = timerMinutes + ":" + extraZero + timerSeconds;
+
+    // check if out of time
+    if (timerSeconds === 0 && timerMinutes === 0) {
+        endExercise();
+    }
+}
+
+function endExercise() {
+    exerciseIsRunning = false;
+    clearInterval(timerR);
+    TIMER_DISPLAY.innerHTML = "0:00";
+
+    document.getElementById("ootHeader").innerHTML = "Time's up!";
+    document.getElementById("finalCorrect").innerHTML = score;
+    document.getElementById("finalTotal").innerHTML = totalQuestions;
+    document.getElementById("outOfTime").style.display = "block";
+
+    if (!previousRecordNotes) {
+        document.getElementById("noRecord").style.display = "block";
+        document.getElementById("noPreviousRecordValue").innerHTML = score;
+        localStorage.setItem("previousRecordNotes", score);
+    } else if (previousRecordNotes < score) {
+        document.getElementById("beatRecord").style.display = "block";
+        document.getElementById("beatPreviousRecordValue").innerHTML = previousRecordNotes;
+        localStorage.setItem("previousRecordNotes", score);
+    } else {
+        document.getElementById("lostRecord").style.display = "block";
+        document.getElementById("lostPreviousRecordValue").innerHTML = previousRecordNotes;
+    }
+}
+
+updateTimer();
+
+// create a variable to allow clearInterval to work
+var timerR = setInterval(updateTimer, TIMER_TICK_MS);
 
 drawStrings();
-startExercise();
-stage.add(circleLayer);
-
-// function answerButton(link) {
-//     if (exerciseIsRunning) {
-
-//         var answer = link.innerHTML;
-
-//         if (currentNote == answer) {
-//             score += 1;
-//             FEEDBACK_DISPLAY.innerHTML = "Correct!";
-//         } else {
-//             FEEDBACK_DISPLAY.innerHTML = "Incorrect!";
-//         }
-
-//         displayFeedback();
-//         totalQuestions += 1;
-//         TOTAL_DISPLAY.innerHTML = totalQuestions;
-//         SCORE_DISPLAY.innerHTML = Math.round((score / totalQuestions) * 100);
-//         CORRECT_DISPLAY.innerHTML = score;
-
-//         startExercise();
-
-//     }
-// }
-
-// function displayFeedback() {
-//     var classToAdd;
-//     var timeout = 1250;
-
-//     if (FEEDBACK_DISPLAY.innerHTML == "Correct!") {
-//         classToAdd = "correctAnswer";
-//     } else {
-//         classToAdd = "incorrectAnswer";
-//     }
-
-//     setTimeout(function() {
-//         FEEDBACK_DIV.classList.remove(classToAdd);
-//         FEEDBACK_DIV.style.opacity = 0;
-//     }, timeout);
-
-//     FEEDBACK_DIV.style.opacity = 1;
-//     FEEDBACK_DIV.classList.add(classToAdd);
-// }
-
-// function updateTimer() {
-//     var extraZero = 0;
-
-//     timerSeconds -= 1;
-
-//     if (timerSeconds < 0) {
-//         timerMinutes -= 1;
-//         timerSeconds = 59;
-//     }
-
-//     if (timerSeconds < 10) {
-//         extraZero = 0;
-//     } else {
-//         extraZero = "";
-//     }
-
-//     TIMER_DISPLAY.innerHTML = timerMinutes + ":" + extraZero + timerSeconds;
-
-//     // check if out of time
-//     if (timerSeconds === 0 && timerMinutes === 0) {
-//         endExercise();
-//     }
-// }
-
-// function endExercise() {
-//     exerciseIsRunning = false;
-//     clearInterval(timerR);
-//     TIMER_DISPLAY.innerHTML = "0:00";
-
-//     document.getElementById("ootHeader").innerHTML = "Time's up!";
-//     document.getElementById("finalCorrect").innerHTML = score;
-//     document.getElementById("finalTotal").innerHTML = totalQuestions;
-//     document.getElementById("outOfTime").style.display = "block";
-
-//     if (!previousRecordNotes) {
-//         document.getElementById("noRecord").style.display = "block";
-//         document.getElementById("noPreviousRecordValue").innerHTML = score;
-//         localStorage.setItem("previousRecordNotes", score);
-//     } else if (previousRecordNotes < score) {
-//         document.getElementById("beatRecord").style.display = "block";
-//         document.getElementById("beatPreviousRecordValue").innerHTML = previousRecordNotes;
-//         localStorage.setItem("previousRecordNotes", score);
-//     } else {
-//         document.getElementById("lostRecord").style.display = "block";
-//         document.getElementById("lostPreviousRecordValue").innerHTML = previousRecordNotes;
-//     }
-// }
-
-// updateTimer();
-
-// // create a variable to allow clearInterval to work
-// var timerR = setInterval(updateTimer, TIMER_TICK_MS);
+drawRandomNote();
